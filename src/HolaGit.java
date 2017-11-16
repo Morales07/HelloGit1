e2c7a44765004f699994e10ab0ddb778995faf32
David Arreola <david.ludan10@outlook.com>
2017-11-16 09:45:46 -0700

    Metodo tabla 12
    Se crea un metodo para generar la tabla del 12, la cual se guarda en una linkedList.

--- src/HolaGit.java
+++ src/HolaGit.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class HolaGit {
@@ -6,7 +7,8 @@ public class HolaGit {
         //Esta es una linea editada por Fco Navarro
         int Git = 0;
     }
-public void dividir() {
+
+    public void dividir() {
     Scanner reader = new Scanner(System.in);
     Scanner m = new Scanner(System.in);
     int numero1 = 2;
@@ -18,7 +20,9 @@ public void dividir() {
     System.out.println("Dame un numero");
     numero2 = m.nextInt();
     System.out.println("La divicion de: " + numero1 + "," + numero2 + "es:" + numero1 / numero2);
-}
+
+    }
+
     public void potencia(int base, int exponente) {
         int resultado = (int) Math.pow(base, exponente);
         System.out.println("La potenia de " + base + " ^ " + exponente + " es: " + resultado);
